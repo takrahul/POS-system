eac5864e8e56f0a673edcc2451f052e6a5d443c8
takrahul <rahultak07@gmail.com>
2019-04-09 13:18:00 +0200

    Operator Class

---
+++ operator/operator.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class OperatorWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class OperatorApp(App):
+    def builf(self):
+        return OperatorWindow()
+
+if __name__=="__main__":
+    oa = OperatorApp()
+    oa.run()
+    
